23c5561e30d8c91441e8af148dc823bae89a7c38
Bhaskar Choudhuri <bhaskar.choudhuri@outlook.com>
2022-07-31 11:43:14 +0530

    Preprocessing moved to separate class -31/7/22

--- PyNaiveBayes.py
+++ PyNaiveBayes.py
@@ -1,5 +1,4 @@
-import nltk, os, os.path
-import datetime
+import nltk, os, os.path, datetime
 import pandas as pd
 from nltk.stem import PorterStemmer
 from nltk.stem import LancasterStemmer
@@ -10,41 +9,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from docx import Document
 from openpyxl import load_workbook
+from clsBhasTextMine import clsBhasTextMine
 
 #nltk.download('averaged_perceptron_tagger')
-def myTextPreProcessor(text):
-    stopWords = set(nltk.corpus.stopwords.words('english'))
-    tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')  # Get tokenizer with builtin cleaning
-    text = tokenizer.tokenize(text)
-    for word in text:
-        if word in stopWords:
-            text.remove(word)  # remove stopwords
-        elif not word.isalpha():
-            text.remove(word)  # remove special characters
-        elif word.isnumeric():
-            text.remove(word)
-    #print (text)
-    #for word, pos in nltk.pos_tag(text):
-    #    if (pos == 'NNP' or pos == 'NNPS' or pos == 'PRP' or pos == 'CD'):
-    #        text.remove(word)
-    #print(text)
-    # texttoken=[]
-    # porter = PorterStemmer()
-    # lancaster = LancasterStemmer()
-    # for word in text:
-    #    texttoken.append(lancaster.stem(word))
+filepath = "d:\BhaskarCode\journal\dataNBtest"
+excelpath = "d:\BhaskarCode\journal\myNaiveBayes0722.xlsx"
+txtpreproc = clsBhasTextMine()  #calling common preprocessing function from class
+timestamp =str(datetime.datetime.now().day)+str(datetime.datetime.now().hour)+ str(datetime.datetime.now().minute)
 
-    return text
-
-def myReadDocx(fullfilepath):
-    doc = Document(fullfilepath)
-    data = ''
-    for para in doc.paragraphs:
-        data = data + ' ' + para.text
-    return data
-
-filepath = "d:\BhaskarCode\FILES\TextMining21\dataNBtest"
-excelpath = "d:\BhaskarCode\FILES\TextMining21\myNaiveBayes0521.xlsx"
 if not os.path.isfile(excelpath):
     print("Training Excel Missing,run myModelClassify from pyDataSimulate")
     #result = myModelClassify("c:\BhaskarCode\FILES\TextMining\dataNBtrain", excelpath)
@@ -52,9 +24,6 @@ if not os.path.isfile(excelpath):
 
 #Read classification tabel from excel with updated label classifiers
 dfTrain=pd.read_excel(excelpath)
-#column = df.iloc[:, 0]
-#column = dfTrain[dfTrain.columns[1]]
-#column = column.values.tolist()
 text=[]                                 #Argument to Vectorizer Fit Transform
 labels=[]                               #Argument to Multinomial Naive Bayes
 for index, rows in dfTrain.iterrows():
@@ -67,7 +36,7 @@ train_vectors = train_vectorizer.fit_transform(text)
 #print(train_vectors)
 
 # Check accuracy of the model by splitting train & test
-X_train, X_test, y_train, y_test = train_test_split(train_vectors, labels, test_size=0.35, random_state=69)
+X_train, X_test, y_train, y_test = train_test_split(train_vectors, labels, test_size=0.60, random_state=69)
 model = MultinomialNB().fit(X_train, y_train)
 predicted = model.predict(X_test)
 print(accuracy_score(y_test, predicted))
@@ -81,8 +50,8 @@ predictlist = []
 for file in os.listdir(filepath):  # loop through all files in the folder
     fullfilepath = os.path.join(filepath, file)
     if os.path.isfile(fullfilepath):
-        data = myReadDocx(fullfilepath)
-        data = myTextPreProcessor(data)
+        data = txtpreproc.myReadDocx(fullfilepath)
+        data = txtpreproc.myTextPreProcessor(data)
         #Naive Bayes Predict takes a list of list, which is a word count of new words in old vector
         testfeature = []
         for word in train_vectorizer.get_feature_names():
@@ -93,10 +62,10 @@ for file in os.listdir(filepath):  # loop through all files in the folder
     else:
         continue
 
-print(*predictlist, sep = "\n")
-#quit()
-timestamp =str(datetime.datetime.now().day)+str(datetime.datetime.now().hour)+ str(datetime.datetime.now().minute)
+#print(*predictlist, sep = "\n")
 dfTestResult = pd.DataFrame(predictlist, columns=['Text', 'Label'])
+#print(dfTestResult)
+
 book = load_workbook(excelpath)
 dfwrite = pd.ExcelWriter (excelpath)
 dfwrite.book = book
@@ -132,3 +101,37 @@ model = MultinomialNB().fit(X_train, y_train)
 predicted = model.predict(X_test)
 print(accuracy_score(y_test, predicted))
 #############################
+
+#####  Dt 31/07/22 ######
+#This text preprocessing being a common function is shifted to class clsBhasTextMine
+#This function can be removed from this file, kept for past reference only
+def myTextPreProcessor(text):
+    stopWords = set(nltk.corpus.stopwords.words('english'))
+    tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')  # Get tokenizer with builtin cleaning
+    text = tokenizer.tokenize(text)
+    for word in text:
+        if word in stopWords:
+            text.remove(word)  # remove stopwords
+        elif not word.isalpha():
+            text.remove(word)  # remove special characters
+        elif word.isnumeric():
+            text.remove(word)
+    #print (text)
+    #for word, pos in nltk.pos_tag(text):
+    #    if (pos == 'NNP' or pos == 'NNPS' or pos == 'PRP' or pos == 'CD'):
+    #        text.remove(word)
+    #print(text)
+    # texttoken=[]
+    # porter = PorterStemmer()
+    # lancaster = LancasterStemmer()
+    # for word in text:
+    #    texttoken.append(lancaster.stem(word))
+
+    return text
+
+def myReadDocx(fullfilepath):
+    doc = Document(fullfilepath)
+    data = ''
+    for para in doc.paragraphs:
+        data = data + ' ' + para.text
+    return data
